8eedce1549910fbfcbd504f879112c272730f0bd
RainhowChan <rainhowchan@163.com>
2016-09-23 01:33:11 +0800

    struts2实例首次提交

---
+++ src/com/rainhowchan/domain/User.java
@@ -0,0 +1,96 @@
+package com.rainhowchan.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	private int userID;
+	private String userName;
+	private String logonName;
+	private String sex;
+	private String birthday;
+	private String education;
+	private String telephone;
+	private String interest;
+	private String path;
+	private String filename;
+	private String remark;
+	
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getLogonName() {
+		return logonName;
+	}
+	public void setLogonName(String logonName) {
+		this.logonName = logonName;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getInterest() {
+		return interest;
+	}
+	public void setInterest(String interest) {
+		this.interest = interest;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	@Override
+	public String toString() {
+		return "User [userID=" + userID + ", userName=" + userName
+				+ ", logonName=" + logonName + ", sex=" + sex + ", birthday="
+				+ birthday + ", education=" + education + ", telephone="
+				+ telephone + ", interest=" + interest + ", path=" + path
+				+ ", filename=" + filename + ", remark=" + remark + "]";
+	}
+	
+	
+	
+}
